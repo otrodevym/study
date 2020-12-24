fa07d16c9cb842be907422f770398818125089e2
otrodevym <otrodevym@gmail.com>
2020-12-24 20:39:18 +0900

    (feat) : 이펙티브 자바 5장 학습
    - 아이템 32 : 제네릭과 가변인수를 함께 쓸 때는 신중하라
    - 아이템 33 : 타입 안전 이종 컨테이너를 고려하라

--- lang/[book]effective_java_3E_stusy/chapter5/item32/src/Item32.java
+++ lang/[book]effective_java_3E_stusy/chapter5/item32/src/Item32.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Item32 {
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item32 {
+//    @SafeVarargs
+//    static <T> List<T> flatten(List<? extends T>... lists) {
+    static <T> List<T> flatten(List<List<? extends T>>lists) {
+        List<T> result = new ArrayList<>();
+        for (List<? extends T> list : lists)
+            result.addAll(list);
+        return result;
+    }
+
+    static Annotation getAnnotation(AnnotatedElement element, String annotationTypeName) {
+        Class<?> annotationType = null;
+        try {
+            annotationType = Class.forName(annotationTypeName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return element.getAnnotation(annotationType.asSubclass(Annotation.class));
+
+    }
 }
