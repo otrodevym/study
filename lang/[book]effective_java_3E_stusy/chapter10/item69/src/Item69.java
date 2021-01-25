f34615328164551e4219fbe4ea2a86a7afb5dc76
otrodevym <otrodevym@gmail.com>
2021-01-25 14:45:50 +0900

    (feat) : 이펙티브 자바 10장 학습

---
+++ lang/[book]effective_java_3E_stusy/chapter10/item69/src/Item69.java
@@ -0,0 +1,25 @@
+/**
+ * Project : item69
+ * Create by IntelliJ IDEA
+ * User: otrodevym
+ * Date: 2021/1/25/0025
+ * Time: 오후 12:15:00
+ */
+public class Item69 {
+    public static void main(String[] args) {
+        try {
+            int i = 0;
+            while(true) {
+                range[i++].climb();
+
+            }
+
+            for (Mountain m : range) {
+                m.climb();
+            }
+        }catch (ArrayIndexOutOfBoundsException e)
+        {
+
+        }
+    }
+}
